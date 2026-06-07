2fbf74f222855365c6bf397fa5347a3685ab19c5
wcsudnik <wcsudnik@iu.edu>
2026-06-06 21:43:44 -0400

    Finished PID loop but no implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-Practice PID loop that could become the basis of my loop for my plane project
-Following the guide online from the Simon Bertoni Control lab
+Practice PID loop that could become the basis of my loop for my projects
+Following the guide online from the Simone Bertoni Control lab
+
+I annotated things I may not understand completely. While I understand the architecture, I would like to better understand some of the finer details.
--- main.c
+++ main.c
@@ -21,3 +21,89 @@ struct PID {
     float prev_command_sat; // previous saturated command
     float prev_command; // previous command
 };
+
+struct Object{
+    float m;    //Mass of the object
+    float k;    // dampening constant of the object
+    float F_Max; //Max force applied
+    float F_Min; //Min force applied
+    float T; //time step
+    float V; //Velocity
+    float Z; //Position
+};
+
+float PID_Step(struct PID *pid, float measurement, float setpoint){
+
+    /*
+    Inputs: pointer to PID struct, current measurement 
+    of the process variable, and the desired setpoint 
+    for the process variable.
+    
+    Outputs: command_sat(saturated command) which is the output
+    command of the PID controller. It is saturated by the min, max,
+    and max_rate vars found in the PID struct
+    */
+    float err;
+    float command;
+    float command_sat;
+    float deriv_filt;
+
+    err = setpoint - measurement;
+    float AntiWindup = pid->Kaw*(pid->prev_command_sat-pid->prev_command)*pid->T;
+
+    pid->integral += pid->Ki*err*pid->T + AntiWindup;
+
+    deriv_filt = (err - pid->prev_err + pid->prev_deriv) / (pid->T + pid->T_C); // I dont understand derivative calculation tbh
+
+    command = pid->Kp * err + pid->integral + pid->Kd * deriv_filt;
+
+    pid->prev_command = command; 
+
+    if (command > pid->max){
+        command = pid->max;
+    }
+    else if (command < pid->min){
+        command = pid->min;
+    }
+    else{
+        command_sat = command;
+    }
+    if (command_sat > pid->prev_command_sat + pid->max_rate * pid->T){
+        command_sat = pid->prev_command_sat + pid->T;
+    }
+    else if (command_sat < pid->prev_command_sat - pid->max_rate * pid->T){
+        command_sat = pid->prev_command_sat - pid->T;
+    }
+    else{}
+    return command_sat;
+
+}
+
+float Object_step(struct Object *obj, float F){
+    /*
+    Inputs: takes in the Object struct and the force 
+    currently applied to the object
+    Outputs: Position in meters using var Z
+    */
+   float dv_dt;
+   float F_sat;
+
+   if (F > obj->F_Max){
+    F_sat = obj->F_Max;
+   }
+   else if (F < obj->F_Min){
+    F_sat = obj->F_Min;
+   }
+   else {F_sat = F;}
+
+   dv_dt = (F_sat - obj->k * obj->V) / obj->m; //THIS IS A NEWTONS FIRST LAW EQUATION YOOO
+
+   obj->V += dv_dt * obj->T;
+   obj->Z += obj->V * obj->T;
+
+   return obj->Z;
+}
+
+int main(){
+    
+}
